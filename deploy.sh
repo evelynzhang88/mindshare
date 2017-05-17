fdd5ee2e548af9e82f5b2dbacda84ecd37c75f58
Eric Lee <ericstone.dev@gmail.com>
2017-05-17 23:57:37 +0800

    add docker files

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+set -e
+
+PRO_SRV='aliyun_wechat'
+PRO_WWW='/mnt/acs_mnt/nas/WeChat002/websites/mindshare.e0x233.com'
+
+rsync \
+  --exclude=.DS_Store \
+  --exclude=.git/ \
+  --exclude=.gitignore \
+  --exclude=.idea \
+  --exclude=.vagrant \
+  --exclude=*.box\
+  --exclude=*.sql\
+  --exclude=*.tgz\
+  --exclude=*.zip\
+  --exclude=deploy.sh \
+  --exclude=docker/ \
+  --delete \
+  -arvP . $PRO_SRV:$PRO_WWW
